5be892b37e3afbb472d63463665a75c7e3c9e5a6
elaad24 <elaad2408@gmail.com>
2024-09-04 21:07:08 +0300

    implemnt signals that updates votes table

---
+++ backend/api/signals.py
@@ -0,0 +1,34 @@
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from .models.votesModel import VotesModel
+from.models.VoteUserModel import UserVotesModel
+
+@receiver(post_save,sender=UserVotesModel)
+def update_vote_count_in_vote_model_by_voteUserModel(sender, instance, created, **kwargs):
+    triggered_instance=instance
+
+    all_instance = UserVotesModel.objects.filter(
+        room_key=triggered_instance.room_key,
+        active_song_id=triggered_instance.active_song_id
+        )
+    
+    liked_instances= list(filter(lambda i : i.vote_type=="1",all_instance))
+    disliked_instance_num=len(all_instance)-len(liked_instances)
+    votes_table_instance=VotesModel.objects.filter(room_key=triggered_instance.room_key)[0]
+    votes_table_instance.like=len(liked_instances)
+    votes_table_instance.dislike=disliked_instance_num
+    votes_table_instance.save()
+
+
+
+
+@receiver(pre_save,sender=VotesModel)
+def cleanup_in_UserVotesModel_when_song_change(sender, instance,**kwargs):
+
+    DB_instance=VotesModel.objects.filter(room_key=instance.room_key)
+
+    if len(DB_instance)>=1  and DB_instance[0].active_song_id !=instance.active_song_id:
+        UserVotesModel.filter(room_key=instance.room_key , active_song_id=instance.active_song_id).delete()
+      
+
+
