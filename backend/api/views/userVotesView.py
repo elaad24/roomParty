47a604b210f93dc85e436b91b9a5637500265ec7
elaad24 <elaad2408@gmail.com>
2024-09-05 17:03:40 +0300

    created post and put function for update the votes

--- backend/api/views/userVotesView.py
+++ backend/api/views/userVotesView.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from api.authentication import CookieJWTAuthentication
 from api.models.VoteUserModel import UserVotesModel
 from api.serializers.userVotes_serializer import UserVotesSerializer
+from api.models.roomModel import RoomModel
 
 
 class UserVotesView(mixins.CreateModelMixin,mixins.UpdateModelMixin, generics.GenericAPIView):
@@ -13,13 +14,38 @@ class UserVotesView(mixins.CreateModelMixin,mixins.UpdateModelMixin, generics.Ge
     permission_classes=[IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        username = request.user.username
+        room_key_field_value = request.data.get("room_key",None)
+        active_song_id_field_value = request.data.get("active_song_id",None)
+        vote_type_value=request.data.get("vote_type",None)
+        user_in_room=request.user.room
+        room_instance=RoomModel.objects.filter(code=user_in_room,active_song_id=active_song_id_field_value).first()
+
+        #checked that the user in this spesific room and the song he send req is current
+        if room_instance != None:
+            UserVotesModel_instance=UserVotesModel.objects.filter(room_key=room_key_field_value , active_song_id=active_song_id_field_value, username=username).first()
+            #there isn't instance - its a new vote 
+            if not UserVotesModel_instance:
+               return self.create(request, *args, **kwargs)
+            #there is a vote and its the save
+            elif UserVotesModel_instance.vote_type==vote_type_value:
+                print(2)
+                return Response({"message":"same vote already existing"},status=status.HTTP_200_OK)
+            #there is a vote and its not the same 
+            elif UserVotesModel_instance and UserVotesModel_instance.vote_type!=vote_type_value:
+                UserVotesModel_instance.vote_type=vote_type_value
+                UserVotesModel_instance.save()
+                return Response({"message":"vote updated"},status=status.HTTP_201_CREATED)
+        #the user not in the room_key / it's not the active song
+        else:
+            return Response({"message":"error couldn't find the instance,user isn't in the room song id isn't active"},status=status.HTTP_400_BAD_REQUEST)
 
 
     def put(self, request, *args, **kwargs):
         username = request.user.username
         room_key_field_value = request.data.get("room_key",None)
         active_song_id_field_value = request.data.get("active_song_id",None)
+        vote_type_value=request.data.get("vote_type",None)
         instance = self.queryset.filter(username=username,
         room_key=room_key_field_value,
         active_song_id=active_song_id_field_value).first()
