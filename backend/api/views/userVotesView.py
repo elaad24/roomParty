a22cc8f0d5b5857de0e1cb7ef40e130dfb6d2c7c
elaad24 <elaad2408@gmail.com>
2024-09-23 19:43:22 +0300

    clear up

--- backend/api/views/userVotesView.py
+++ backend/api/views/userVotesView.py
@@ -1,50 +1,81 @@
-from rest_framework import generics,status,mixins
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from api.authentication import CookieJWTAuthentication
+from api.models.roomModel import RoomModel
 from api.models.VoteUserModel import UserVotesModel
 from api.serializers.userVotes_serializer import UserVotesSerializer
-from api.models.roomModel import RoomModel
+from rest_framework import generics, mixins, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
-class UserVotesView(mixins.CreateModelMixin,mixins.UpdateModelMixin, generics.GenericAPIView):
-    queryset=UserVotesModel.objects.all()
+class UserVotesView(
+    mixins.CreateModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView
+):
+    queryset = UserVotesModel.objects.all()
     serializer_class = UserVotesSerializer
-    authentication_classes=[CookieJWTAuthentication]
-    permission_classes=[IsAuthenticated]
+    authentication_classes = [CookieJWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         username = request.user.username
-        room_key_field_value = request.data.get("room_key",None)
-        active_song_id_field_value = request.data.get("active_song_id",None)
-        vote_type_value=request.data.get("vote_type",None)
-        user_in_room=request.user.room
-        room_instance=RoomModel.objects.filter(code=user_in_room,active_song_id=active_song_id_field_value).first()
+        room_key_field_value = request.data.get("room_key", None)
+        active_song_id_field_value = request.data.get("active_song_id", None)
+        vote_type_value = request.data.get("vote_type", None)
+        user_in_room = request.user.room
+        room_instance = RoomModel.objects.filter(
+            code=user_in_room, active_song_id=active_song_id_field_value
+        ).first()
 
-        #checked that the user in this spesific room and the song he send req is current
+        # checked that the user in this spesific room and the song he send req is current
         if room_instance != None:
-            UserVotesModel_instance=UserVotesModel.objects.filter(room_key=room_key_field_value , active_song_id=active_song_id_field_value, username=username).first()
-            #there isn't instance - its a new vote 
+            UserVotesModel_instance = UserVotesModel.objects.filter(
+                room_key=room_key_field_value,
+                active_song_id=active_song_id_field_value,
+                username=username,
+            ).first()
+            # there isn't instance - its a new vote
             if not UserVotesModel_instance:
-                UserVotesModel.objects.create(room_key=room_key_field_value,username=request.user.username,active_song_id=active_song_id_field_value,vote_type=vote_type_value)
-                return Response({},status=status.HTTP_201_CREATED)
-            #there is a vote and its the same - so it remove that comment 
-            elif UserVotesModel_instance.vote_type==vote_type_value:
+                UserVotesModel.objects.create(
+                    room_key=room_key_field_value,
+                    username=request.user.username,
+                    active_song_id=active_song_id_field_value,
+                    vote_type=vote_type_value,
+                )
+                return Response({}, status=status.HTTP_201_CREATED)
+            # there is a vote and its the same - so it remove that comment
+            elif UserVotesModel_instance.vote_type == vote_type_value:
                 UserVotesModel_instance.delete()
-                return Response({"message":"the vote has been deleted"},status=status.HTTP_202_ACCEPTED)
-            #there is a vote and its not the same 
-            elif UserVotesModel_instance and UserVotesModel_instance.vote_type!=vote_type_value:
-                UserVotesModel_instance.vote_type=vote_type_value
+                return Response(
+                    {"message": "the vote has been deleted"},
+                    status=status.HTTP_202_ACCEPTED,
+                )
+            # there is a vote and its not the same
+            elif (
+                UserVotesModel_instance
+                and UserVotesModel_instance.vote_type != vote_type_value
+            ):
+                UserVotesModel_instance.vote_type = vote_type_value
                 UserVotesModel_instance.save()
-                return Response({"message":"vote updated"},status=status.HTTP_201_CREATED)
-        #the user not in the room_key / it's not the active song
+                return Response(
+                    {"message": "vote updated"}, status=status.HTTP_201_CREATED
+                )
+        # the user not in the room_key / it's not the active song
         else:
-            return Response({"message":"error couldn't find the instance,user isn't in the room song id isn't active"},status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(
+                {
+                    "message": "error couldn't find the instance,user isn't in the room song id isn't active"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
     def get(self, request, *args, **kwargs):
         username = request.user.username
-        user_vote_instance= UserVotesModel.objects.filter(username=username).first()
-        if user_vote_instance==None:
-            return Response({"vote":None},status=status.HTTP_200_OK)
-        return Response({"vote":user_vote_instance.vote_type,"active_song_id":user_vote_instance.active_song_id},status=status.HTTP_200_OK)
+        user_vote_instance = UserVotesModel.objects.filter(username=username).first()
+        if user_vote_instance == None:
+            return Response({"vote": None}, status=status.HTTP_200_OK)
+        return Response(
+            {
+                "vote": user_vote_instance.vote_type,
+                "active_song_id": user_vote_instance.active_song_id,
+            },
+            status=status.HTTP_200_OK,
+        )
--- backend/spotify/utils.py
+++ backend/spotify/utils.py
@@ -1,41 +1,48 @@
-from .models import SpotifyToken
-from django.utils import timezone
 from datetime import timedelta
-from .credenials import CLIENT_ID ,CLIENT_SECRET
-from requests import post, put,get
+
+from api.models.customUserModel import CustomUserModel
+from django.utils import timezone
+from requests import get, post, put
+
+from .credenials import CLIENT_ID, CLIENT_SECRET
+from .models import SpotifyToken
 
 BASE_URL = "https://api.spotify.com/v1/"
 
-def update_or_create_user_token(username,refresh_token,access_token,token_type):
-    expires_in=timezone.now()+timedelta(seconds=3600) #set the the expires is in 1 hour from now
-    user_token=SpotifyToken.objects.filter(username=username).first()
-    
+
+def update_or_create_user_token(username, refresh_token, access_token, token_type):
+    expires_in = timezone.now() + timedelta(
+        seconds=3600
+    )  # set the the expires is in 1 hour from now
+    user_token = SpotifyToken.objects.filter(username=username).first()
+
     # user_token=SpotifyToken.objects.all().delete()
 
-    if user_token==None:
+    if user_token == None:
         # create
-        token=SpotifyToken.objects.create(
-        username=username,
-        refresh_token=refresh_token,
-        access_token=access_token,
-        expires_in=expires_in,
-        token_type=token_type
-        ).save() 
+        token = SpotifyToken.objects.create(
+            username=username,
+            refresh_token=refresh_token,
+            access_token=access_token,
+            expires_in=expires_in,
+            token_type=token_type,
+        ).save()
         return token.user_token_id
     else:
-        # update 
+        # update
 
-        user_token.username=username
-        user_token.access_token=access_token
-        user_token.refresh_token=refresh_token
-        user_token.expires_in=expires_in
-        user_token.token_type=token_type
+        user_token.username = username
+        user_token.access_token = access_token
+        user_token.refresh_token = refresh_token
+        user_token.expires_in = expires_in
+        user_token.token_type = token_type
         user_token.save()
         return user_token.user_token_id
 
-def set_username_in_spotify(token_id,username):
-    user_instance=  SpotifyToken.objects.filter(user_token_id=token_id).first()
-    if user_instance == None :
+
+def set_username_in_spotify(token_id, username):
+    user_instance = SpotifyToken.objects.filter(user_token_id=token_id).first()
+    if user_instance == None:
         return False
     else:
         user_instance.username = username
@@ -44,41 +51,49 @@ def set_username_in_spotify(token_id,username):
 
 
 def is_spotify_authenticated(username):
-    token=SpotifyToken.objects.filter(username=username).first()
+    token = SpotifyToken.objects.filter(username=username).first()
     if token:
         expiry = token.expires_in
         if expiry <= timezone.now():
             refresh_spotify_token(username)
         return True
-    
+
     return False
 
 
 def refresh_spotify_token(username):
     refresh_token = SpotifyToken.objects.filter(username=username).first().refresh_token
 
-    response = post('https://accounts.spotify.com/api/token', data={
-    'grant_type': 'refresh_token',
-    'refresh_token': refresh_token,
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET
-    }).json()
+    response = post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    ).json()
 
-    access_token = response.get('access_token')
-    token_type = response.get('token_type')
+    access_token = response.get("access_token")
+    token_type = response.get("token_type")
 
-    update_or_create_user_token(username,refresh_token,access_token,token_type)
+    update_or_create_user_token(username, refresh_token, access_token, token_type)
+
 
 def get_user_tokens(username):
     spotifyUserInstance = SpotifyToken.objects.filter(username=username).first()
     if spotifyUserInstance != None:
         return spotifyUserInstance
-    else: 
+    else:
         return None
 
-def execute_spotify_api_request(room_host_username,endpoint, post_=False,put_=False):
+
+def execute_spotify_api_request(room_host_username, endpoint, post_=False, put_=False):
     tokens = get_user_tokens(room_host_username)
-    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + tokens.access_token}
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + tokens.access_token,
+    }
     if post_:
         post(BASE_URL + endpoint, headers=headers)
     if put_:
@@ -88,4 +103,4 @@ def execute_spotify_api_request(room_host_username,endpoint, post_=False,put_=Fa
     try:
         return response.json()
     except:
-        return {'Error': 'Issue with request'}
+        return {"Error": "Issue with request"}
