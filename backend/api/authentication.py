f2e569d005162fff82ec108f4cb3fe91c00e02d2
elaad24 <elaad2408@gmail.com>
2024-09-25 14:57:35 +0300

    added routh for deleting the room queue

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -1,21 +1,21 @@
-from rest_framework.authentication import BaseAuthentication
-from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth import get_user_model
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import AccessToken
 
 
 class CookieJWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        access_token = request.COOKIES.get('access_token')
+        access_token = request.COOKIES.get("access_token")
         if not access_token:
             return None
-        
+
         try:
-            #validate the token 
+            # validate the token
             validated_token = AccessToken(access_token)
-        
             User = get_user_model()
-            current_user = User.objects.get(id= validated_token['user_id'])
-            return (current_user,None)
-        except Exception as e: 
+            current_user = User.objects.get(id=validated_token["user_id"])
+            request.userData = current_user
+            return (current_user, None)
+        except Exception as e:
             raise AuthenticationFailed("Invalid or expired token.")
---
+++ backend/api/views/deleteQueue.py
@@ -0,0 +1,29 @@
+from api.authentication import CookieJWTAuthentication
+from api.models.customUserModel import CustomUserModel
+from api.models.songs_queue import SongsQueueModel
+from api.serializers.songs_queue_serializer import Songs_queue_serializer
+from api.utils.songQueueUtils import add_song_to_queue
+from rest_framework import generics, mixins, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+
+class delateQueue(generics.ListAPIView):
+    queryset = SongsQueueModel.objects.all()
+    serializer_class = Songs_queue_serializer
+    authentication_classes = [CookieJWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+
+        user = request.user
+        user_instance = CustomUserModel.objects.filter(username=user.username).first()
+
+        song_queue_instate = SongsQueueModel.objects.filter(
+            room_key=user_instance.room
+        ).delete()
+        if song_queue_instate != None:
+            return Response({"data": "deleted"}, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "didnt find items"}, status=status.HTTP_400_BAD_REQUEST
+        )
